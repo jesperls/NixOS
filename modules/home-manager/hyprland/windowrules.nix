fb91fac4a28f2fdc20460f40daaa77ff9aa17f5c
jesperls <jesper.ls@hotmail.com>
2026-01-16 15:51:33 +0100

    bogus binted

--- modules/home-manager/hyprland/windowrules.nix
+++ modules/home-manager/hyprland/windowrules.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   windowrulev2 = [
+    # === Floating Windows ===
     "float,class:^(pavucontrol)$"
     "float,class:^(blueman-manager)$"
     "float,class:^(nm-connection-editor)$"
@@ -9,8 +10,35 @@
     "float,class:^(eog)$"
     "float,class:^(vlc)$"
     "float,class:^(imv)$"
+    "float,class:^(feh)$"
+    "float,class:^(file-roller)$"
+    "float,class:^(qpwgraph)$"
+    "float,class:^(org.pulseaudio.pavucontrol)$"
+    "float,class:^(solaar)$"
+    "float,class:^(overskride)$"
+    "float,class:^(scrcpy)$"
+    "float,title:^(Open File)$"
+    "float,title:^(Save File)$"
+    "float,title:^(Confirm to replace files)$"
+    "float,title:^(File Operation Progress)$"
+    
     "float,title:^(Picture-in-Picture)$"
     "pin,title:^(Picture-in-Picture)$"
-    "opacity 0.9 0.9,class:^(kitty)$"
+    "keepaspectratio,title:^(Picture-in-Picture)$"
+    "size 480 270,title:^(Picture-in-Picture)$"
+    "move 100%-490 100%-280,title:^(Picture-in-Picture)$"
+
+    "opacity 0.92 0.88,class:^(kitty)$"
+    
+    
+    "float,class:^(xdg-desktop-portal-gtk)$"
+    "float,class:^(polkit-gnome-authentication-agent-1)$"
+    "stayfocused,class:^(polkit-gnome-authentication-agent-1)$"
+    
+    "opacity 1.0 override 1.0 override,title:^(.*)(Sharing your screen)(.*)$"
+    "opacity 1.0 override 1.0 override,title:^(.*)(sharing indicator)(.*)$"
+    
+    "float,class:^(thunar)$,title:^(File Operation Progress)$"
+    "float,class:^(thunar)$,title:^(Confirm to replace files)$"
   ];
 }
